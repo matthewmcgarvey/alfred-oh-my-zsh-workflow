4b6a7f41dd5a6582cf8e0fc032ce1797837411dd
Matthew McGarvey <tnraptor14@hotmail.com>
2019-02-12 22:25:43 -0600

    add refresh

--- oh-my-zsh.py
+++ oh-my-zsh.py
@@ -2,17 +2,34 @@
 import sys
 from workflow import Workflow3, ICON_WEB, web
 
-def get_data():
+def get_plugins():
     url = 'https://api.github.com/repos/robbyrussell/oh-my-zsh/contents/plugins'
     r = web.get(url)
     r.raise_for_status()
     return r.json()
 
-def get_cache(wf):
-    return wf.cached_data('data', get_data, max_age=0)
+def get_data(wf):
+    return wf.stored_data('data')
+
+def save_data(wf, data):
+    wf.store_data('data', data)
+
+def clear_data(wf):
+    wf.clear_data()
 
 def main(wf):
-    for content in get_cache(wf):
+    if len(wf.args) and wf.args[0] == 'refresh':
+        clear_data(wf)
+        save_data(wf, get_plugins())
+        return
+    
+    data = get_data(wf)
+
+    if data is None:
+        data = get_plugins()
+        save_data(wf, data)
+
+    for content in data:
         if content['type'] == 'dir':
             url = content['html_url']
             wf.add_item(title=content['name'], subtitle=url, arg=url, icon=ICON_WEB, valid=True)
